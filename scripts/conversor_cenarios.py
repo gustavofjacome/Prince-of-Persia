62614bf82343b231155996d902b228c37ef9d7ab
Gustavo Jácome <155338841+gustavofjacome@users.noreply.github.com>
2026-05-24 17:49:12 -0300

    feat: adicionar script de conversão PNG para ASM

---
+++ scripts/conversor_cenarios.py
@@ -0,0 +1,60 @@
+from PIL import Image
+
+# ==========================================
+# CONFIGURAÇÕES
+# ==========================================
+
+INPUT_IMAGE = "cenario2.png" #colocar o nome do arquivo .png que sera convertido
+OUTPUT_FILE = "stage2.asm" #colocar o nome do arquivo .asm que sera gerado
+
+BASE_LABEL = "cenario_2" # configurar corretamenteo o valor do rotulo que estara configurado no asm gerado
+
+TRANSPARENT_COLOR = "0xFFFFFFFF"
+
+img = Image.open(INPUT_IMAGE).convert("RGBA")
+
+width, height = img.size
+
+with open(OUTPUT_FILE, "w") as f:
+
+    f.write(".data\n\n") #declarando o .data
+
+    f.write(f"{BASE_LABEL}_width: .word {width}\n")
+    f.write(f"{BASE_LABEL}_height: .word {height}\n\n")
+
+    f.write(f"{BASE_LABEL}:\n")
+
+    for y in range(height):
+
+        f.write(f"\n    # linha {y}\n")
+
+        row_pixels = []
+
+        for x in range(width):
+
+            r, g, b, a = img.getpixel((x, y))
+
+            if a == 0:
+                row_pixels.append(TRANSPARENT_COLOR)
+
+            else:
+                hex_color = (r << 16) | (g << 8) | b
+                row_pixels.append(f"0x{hex_color:06X}")
+
+        # print compactado para economizar linhas, (antes tinha 50k)
+        f.write("    .word ")
+        f.write(", ".join(row_pixels))
+        f.write("\n")
+
+
+total_pixels = width * height
+
+print("===================================")
+print("ASM gerado com sucesso!")
+print("===================================")
+print(f"Imagem: {INPUT_IMAGE}")
+print(f"Saída : {OUTPUT_FILE}")
+print(f"Largura : {width}")
+print(f"Altura  : {height}")
+print(f"Pixels  : {total_pixels}")
+print("===================================")
---
+++ scripts/conversor_sprites.py
@@ -0,0 +1,37 @@
+from PIL import Image
+
+INPUT = "prince_jump_right.png"
+OUTPUT = "prince_jump_right.asm"
+LABEL = "prince_jump_right"
+TILE_SIZE = None  # usa imagem inteira 
+
+def rgb_to_hex(r, g, b):
+    return (r << 16) | (g << 8) | b
+
+img = Image.open(INPUT).convert("RGBA")
+w, h = img.size
+
+pixels = []
+
+for y in range(h):
+    row = []
+    for x in range(w):
+        r, g, b, a = img.getpixel((x, y))
+
+        
+        if a < 10:
+            hex_color = 0x00000000
+        else:
+            hex_color = rgb_to_hex(r, g, b)
+
+        row.append(f"0x{hex_color:08X}")
+    pixels.append(row)
+
+with open(OUTPUT, "w") as f:
+    f.write(".data\n\n")
+    f.write(f"{LABEL}:\n")
+
+    for row in pixels:
+        f.write(".word " + ", ".join(row) + "\n")
+
+print(f"Sprite gerado com sucesso em {OUTPUT}")
